9844d2f4f08e2fcf02c0b4b50742922728d51130
JDevGZ <98914608+JDevGZ@users.noreply.github.com>
2022-05-22 20:20:12 -0600

    Final commit with cart and products. Unable to use JSON for products.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
-// import NavBar from './Components/NavBar';
-import ProductList from './Components/ProductList';
-
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Cart from './Components/Cart';
+import data from './data';
+import { useState } from 'react';
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
-    <div>
-      <h1>Product Catalog</h1>
-      <ProductList random={true} cols="5" limit="30" />
+    <div className="App">
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Cart
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Cart>
+      </div>
     </div>
   );
 }
--- src/Routes/ProductById.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {useParams} from "react-router-dom";
-import ProductDetail from '../Components/ProductDetail';
-
-export default function ProductById() {
-  const { productId } = useParams();
-
-  return <ProductDetail id={productId} key={productId} />
-}
